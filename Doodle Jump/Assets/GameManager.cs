6ad423560346dc6a13483ea8b875f1f60c650911
caitac <caicel24@bergen.org>
2024-06-10 07:25:13 -0400

    Editing Colliders

--- Doodle Jump/Assets/GameManager.cs
+++ Doodle Jump/Assets/GameManager.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject platformPrefab; // starting w base code for one platform that i will edit 
+
+    public int platformCount = 300;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        Vector3 spawnPosition = new Vector3();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for (int i = 0; i < platformCount; i++){
+            spawnPosition.y += Random.Range(.5f, 2f);
+            spawnPosition.x = Random.Range(.5f, 5f);
+            Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+        }
     }
 }
