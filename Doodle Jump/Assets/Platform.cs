84b0be056b37ab1abd67b7c97c2f19d0d167d473
caitac <caicel24@bergen.org>
2024-06-10 00:30:45 -0400

    Code so player bounces off platform

--- Doodle Jump/Assets/Platform.cs
+++ Doodle Jump/Assets/Platform.cs
@@ -8,9 +8,17 @@ public class Platform : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision relativeVelocity.y <= 0f)
+        if (collision.relativeVelocity.y <= 0f)
         {
-            R
+            Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();            if (rb != null)
+            {
+                if(rb != null){
+                    Vector2 velocity = rb.velocity;
+                    velocity.y = jumpForce;
+                    rb.velocity = velocity; //when player touches platform it launches them into the air
+                }
+            }
+
         }
     }
 }
