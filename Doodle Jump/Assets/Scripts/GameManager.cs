7dbe01c34d13ac78ca5def17245dbfa5ac4b9b54
caitac <caicel24@bergen.org>
2024-06-11 11:39:30 -0400

    Adding some fish made code mor efficient
    left comments in game manager to do later

--- Doodle Jump/Assets/Scripts/GameManager.cs
+++ Doodle Jump/Assets/Scripts/GameManager.cs
@@ -5,18 +5,33 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject platformPrefab; // starting w base code for one platform that i will edit 
-    public int platformCount = 500; // game stops at this many 
-    // Start is called before the first frame update
+    //public int platformCount = 500; // game stops at this many 
+    Vector3 spawnPosition = new Vector3();
+    
     void Start()
     {
-        Vector3 spawnPosition = new Vector3();
-
-        for (int i = 1; i < platformCount; i++){
-            spawnPosition.y += Random.Range(.5f, 2f);
-            spawnPosition.x = Random.Range(-10f, 10f);
+        //!! set visiblity 
+        
+        //for (int i = 1; i < platformCount; i++){
+          //  spawnPosition.y += Random.Range(.5f, 2f);
+            //spawnPosition.x = Random.Range(-10f, 10f);
             
-            Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+            //Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
 
-        }
+    }
+
+    void Update()
+    {
+        Invoke("AddFish", 2); 
+
+        //check if off screen and delete if yas 
+    }
+
+    public void AddFish()
+    {
+        spawnPosition.y += Random.Range(.5f, 2f);
+        spawnPosition.x = Random.Range(-10f, 10f);
+            
+        Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
     }
 }
